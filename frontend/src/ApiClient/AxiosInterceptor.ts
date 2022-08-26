344a1a49ca8b208cfcaa6c86f85d3f8032e93cb1
Przemyslaw Lenczewski <p.lenczewski@icloud.com>
2022-08-26 20:56:44 +0200

    automatic log in

--- frontend/src/ApiClient/AxiosInterceptor.ts
+++ frontend/src/ApiClient/AxiosInterceptor.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { store } from '../store'
 import AccountRepository from './Account/AccountProxy';
-import { refreshToken as refreshTokenAction } from '../Actions/Account/accountActions';
+import { logOut, refreshToken as refreshTokenAction } from '../Actions/Account/accountActions';
 
 const axiosApiAuthorized = axios.create();
 
