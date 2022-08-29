a9d455df618547931866a8ee0ed35a2c54659e1c
ra11p0 <p.lenczewski@icloud.com>
2022-08-29 21:47:03 +0200

    wip

--- backend/src/Gradebook.Foundation.Common/Identity/Logic/Interfaces/IIdentityLogic.cs
+++ backend/src/Gradebook.Foundation.Common/Identity/Logic/Interfaces/IIdentityLogic.cs
@@ -8,7 +8,10 @@ public interface IIdentityLogic
     JwtSecurityToken CreateToken(List<Claim> authClaims);
     string GenerateRefreshToken();
     ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token);
-    Task EditUserRoles(string userGuid, string[] roles);
-    Task<ResponseWithStatus<string[], bool>> GetUserRoles(string userGuid);
+    Task EditUserRoles(string[] roles, string? userGuid = null);
+    Task<ResponseWithStatus<string[], bool>> GetUserRoles(string? userGuid = null);
     Task<ResponseWithStatus<string, bool>> CurrentUserId();
+    Task<ResponseWithStatus<bool>> AddUserRole(string role, string? userGuid = null);
+    Task<ResponseWithStatus<bool>> RemoveUserRole(string role, string? userGuid = null);
+    Task AddUserRole(object superAdmin);
 }
