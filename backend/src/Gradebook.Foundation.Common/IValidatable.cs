c2042a1a769b0af9d4561b19c9fd4fcdf02a9be0
ra11p0 <p.lenczewski@icloud.com>
2022-09-04 16:43:43 +0200

    refractor

--- backend/src/Gradebook.Foundation.Common/IValidatable.cs
+++
@@ -1,7 +0,0 @@
-namespace Gradebook.Foundation.Common;
-
-public interface IValidatable<Target, Status>
-{
-    public Validator<Target, Status> Validator { get; } 
-    public Status IsValid =>  Validator.Validate((Target) this);
-}
---
+++ backend/src/Gradebook.Foundation.Common/Validatable.cs
@@ -0,0 +1,12 @@
+namespace Gradebook.Foundation.Common;
+
+public abstract class Validatable<Target> where Target : Validatable<Target>
+{
+    private Validator<Validatable<Target> , bool> _validator;
+    public bool IsValid => _validator.Validate(this);
+    protected Validatable()
+    {
+        _validator = new Validator<Validatable<Target>, bool>(e=>Validate((Target) this));
+    }
+    protected abstract bool Validate(Target validatable);
+}
