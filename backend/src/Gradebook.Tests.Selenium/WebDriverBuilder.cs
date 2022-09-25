c13a96144c1f647832f9f3e793d72134329aacd9
ra11p0 <p.lenczewski@icloud.com>
2022-09-25 13:58:59 +0200

    driver fix

--- backend/src/Gradebook.Tests.Selenium/WebDriverBuilder.cs
+++ backend/src/Gradebook.Tests.Selenium/WebDriverBuilder.cs
@@ -8,7 +8,18 @@ public static class WebDriverBuilder
     public static IWebDriver BuildWebDriver()
     {
         ChromeOptions options = new();
-        if (bool.Parse(ConfigurationManager.GetValue("Browser:RunHeadless"))) options.AddArgument("--headless");
+        options.AddArgument("--window-size=1366,768");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--proxy-server='direct://'");
+        options.AddArgument("--proxy-bypass-list=*");
+        options.AddArgument("--start-maximized");
+        options.AddArguments("no-sandbox", "--disable-infobars", "--disable-dev-shm-usage", "--disable-browser-side-navigation", "--ignore-certificate-errors");
+
+        if (bool.Parse(ConfigurationManager.GetValue("Browser:RunHeadless")))
+        {
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--headless");
+        }
         string? path = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
         return new ChromeDriver(path + @"/Drivers/", options);
     }
