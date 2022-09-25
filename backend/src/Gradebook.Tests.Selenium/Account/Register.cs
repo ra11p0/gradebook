63306f43e8219f50e8d7c1af5bca58c54de887ff
ra11p0 <p.lenczewski@icloud.com>
2022-09-25 12:22:40 +0200

    added test project

---
+++ backend/src/Gradebook.Tests.Selenium/Account/Register.cs
@@ -0,0 +1,19 @@
+namespace Gradebook.Tests.Selenium.Account;
+
+public class Register
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    [TearDown]
+    public void End()
+    {
+    }
+}
