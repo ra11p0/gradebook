5cc11df8fd61e426a0af752ccd088b1b037c7c54
ra11p0 <p.lenczewski@icloud.com>
2022-10-06 22:16:01 +0200

    before dnd

--- modules/requests-app/src/Components/Editor/EditorStage.tsx
+++ modules/requests-app/src/Components/Editor/EditorStage.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { ListGroup } from "react-bootstrap";
-import FieldTypes from "../../Constraints/FieldTypes";
+import { DndProvider } from "react-dnd";
 import Field from "../../Interfaces/Common/Field";
 import FieldComponent from "./Fields/Field";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 type Props = {
   fields: Field[];
@@ -12,13 +12,15 @@ type Props = {
 function EditorStage(props: Props) {
   return (
     <div className="d-flex justify-content-center m-2 p-2">
-      <ListGroup className="w-75">
-        {props.fields.map((field, index) => (
-          <ListGroup.Item key={index}>
-            <FieldComponent field={field} onRemoveFieldHandler={() => props.onRemoveFieldHandler(field.uuid)} />
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      <DndProvider backend={HTML5Backend}>
+        <ListGroup className="w-75">
+          {props.fields.map((field, index) => (
+            <ListGroup.Item key={index}>
+              <FieldComponent field={field} onRemoveFieldHandler={() => props.onRemoveFieldHandler(field.uuid)} />
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </DndProvider>
     </div>
   );
 }
