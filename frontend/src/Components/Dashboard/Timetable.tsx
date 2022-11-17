190420371417a2f11ccab22950ffdc9e38f0c4e4
ra11p0 <p.lenczewski@icloud.com>
2022-11-17 21:00:30 +0100

    timetable component

--- frontend/src/Components/Dashboard/Timetable.tsx
+++ frontend/src/Components/Dashboard/Timetable.tsx
@@ -1,9 +1,10 @@
+import moment from "moment";
 import React from "react";
 import { Button, Stack } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import getIsLoggedInReduxProxy from "../../Redux/ReduxProxy/getIsLoggedInReduxProxy";
-import CalendarCollapsible from "../Shared/CalendarCollapsible";
+import CalendarCollapsible from "../Shared/CalendarCollapsible/CalendarCollapsible";
 
 const mapStateToProps = (state: any) => ({});
 
@@ -18,7 +19,39 @@ function Timetable(props: Props) {
       <div className="d-flex justify-content-end m-1 p-1">
         <Button>{t('addEvent')}</Button>
       </div>
-      <CalendarCollapsible />
+      <CalendarCollapsible events={[{
+        startDate: moment('2022-11-18 10:10'),
+        endDate: moment('2022-11-18 12:05'),
+        title: 'Projektowanie portali korporacyjnych; SPEC.: Inżynieria technologii internetowych ',
+      }, {
+        startDate: moment('2022-11-20 12:30'),
+        endDate: moment('2022-11-20 14:25'),
+        title: 'PDW VII: Sztuczna Inteligencja ',
+      }, {
+        startDate: moment('2022-11-19 16:50'),
+        endDate: moment('2022-11-19 18:45'),
+        title: 'PDW VI: Zarządzanie projektami IT',
+      }, {
+        startDate: moment('2022-11-18 19:00'),
+        endDate: moment('2022-11-18 20:55'),
+        title: 'PDW VII: Sztuczna Inteligencja',
+      }, {
+        startDate: moment('2022-11-17 11:20'),
+        endDate: moment('2022-11-17 13:20'),
+        title: 'Zaawansowane technologie internetowe; SPEC.: Inżynieria technologii internetowych ',
+      }, {
+        startDate: moment('2022-11-17 15:20'),
+        endDate: moment('2022-11-17 16:20'),
+        title: 'PDW VIII: Wprowadzenie do prawa i praw autorskich',
+      }, {
+        startDate: moment('2022-11-16 16:20'),
+        endDate: moment('2022-11-16 19:20'),
+        title: 'Projektowanie wielowarstwowych aplikacji internetowych; SPEC.: Inżynieria technologii internetowych',
+      }, {
+        startDate: moment('2022-11-16 15:20'),
+        endDate: moment('2022-11-16 17:20'),
+        title: 'dsadsa',
+      },]} />
     </Stack>
   </>;
 }
---
+++ frontend/src/Components/Shared/CalendarCollapsible/CalendarCollapsible.tsx
@@ -0,0 +1,134 @@
+import { faArrowDown, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment, { Moment } from 'moment';
+import React, { ReactElement, useEffect, useState } from 'react'
+import { Accordion, Button, Card, useAccordionButton, Row, Col, ListGroup, ListGroupItem, Stack } from 'react-bootstrap'
+
+
+type Event = {
+    startDate: Moment;
+    endDate: Moment;
+    title: string;
+};
+
+type DayTileProps = {
+    day: Moment;
+    events: Event[];
+};
+
+function CustomToggle({ children, eventKey, onClick }: { children: any, eventKey: string, onClick: () => void }) {
+    const decoratedOnClick = useAccordionButton(eventKey, onClick);
+
+    return (
+        <Button onClick={decoratedOnClick} variant='outlined' >
+            {children}
+        </Button>
+    );
+}
+
+function DayTile(props: DayTileProps) {
+    return (
+        <Col className='px-1'>
+            <Card className='my-1'>
+                <Card.Header className='fs-5'>
+                    {moment(props.day).local().format('Do MMM')}
+                </Card.Header>
+                <Card.Body className='p-0'>
+                    <ListGroup>
+                        {props.events.sort((o, n) => o.startDate.toDate().getTime() - n.startDate.toDate().getTime()).map((evt, key) => (
+                            <ListGroupItem key={key} >
+                                <Row>
+                                    <Col>
+                                        <Row className='text-center'>
+                                            <Col>
+                                                <small>
+                                                    {`${evt.startDate.local().format('LT')}`}
+                                                </small>
+                                            </Col>
+                                            <Col>
+                                                <small>
+                                                    {`${evt.endDate.local().format('LT')}`}
+                                                </small>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                {evt.title}
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+                </Card.Body>
+            </Card>
+        </Col >
+    );
+}
+
+type Props = { events: Event[] }
+
+function CalendarCollapsible(props: Props) {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const [startDate, setStartDate] = useState(moment());
+    const [withWeekends, setWithWeekends] = useState(true);
+    useEffect(() => {
+        const stopDate = startDate.clone().endOf('day').add(1, 'week').startOf('day');
+        const currentlyShownEvents = props.events.filter(ev => ev.startDate.clone().isBetween(startDate.clone().startOf('day'), stopDate));
+        setWithWeekends(currentlyShownEvents.length == 0 ? false : !!currentlyShownEvents.map(ev => ev.startDate).find(d => d.isoWeekday()));
+    }, [startDate]);
+    return (
+        <>
+            <Accordion>
+                <Card>
+                    <Card.Body className='mb-0 pb-0'>
+                        <Row>
+                            <div className='d-flex justify-content-between'>
+                                <Button variant='outlined' onClick={() => setStartDate(e => e.clone().subtract(7, 'days'))} > <FontAwesomeIcon icon={faArrowLeft} /></Button>
+                                <Button variant='outlined' onClick={() => setStartDate(e => e.clone().add(7, 'days'))} ><FontAwesomeIcon icon={faArrowRight} /></Button>
+                            </div>
+                        </Row>
+                        <Row>
+                            {
+                                [...Array(withWeekends ? 7 : 5)].map((e, i) => (
+                                    <Col className='text-center' key={i}>
+                                        {moment().startOf('isoWeek').add(i, 'days').format('ddd')}
+                                    </Col>))
+                            }
+                        </Row>
+                        <Row>
+                            {
+                                [...Array(withWeekends ? 7 : 5)].map((e, i) => {
+                                    const day = startDate.clone().startOf('isoWeek').add(0 * 7 + i, 'days');
+                                    return <DayTile key={i} day={day} events={props.events.filter(event => event.startDate.format('l') == day.format('l'))} />
+                                })
+                            }
+                        </Row>
+                    </Card.Body>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body className='my-0 py-0'>
+                            <Row>
+                                {
+                                    [...Array(withWeekends ? 7 : 5)].map((e, i) => {
+                                        const day = startDate.clone().startOf('isoWeek').add(1 * 7 + i, 'days');
+                                        return <DayTile key={i} day={day} events={props.events.filter(event => event.startDate.format('l') == day.format('l'))} />
+                                    })
+                                }
+                            </Row>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                    <Card.Body className='mt-0 pt-0'>
+                        <div className='d-flex justify-content-end'>
+                            <CustomToggle eventKey={"0"} onClick={() => setIsCollapsed(c => !c)} >
+                                <FontAwesomeIcon icon={faArrowDown} className={isCollapsed ? "" : "rotate-180"} />
+                            </CustomToggle>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Accordion>
+        </>
+    )
+}
+
+export default CalendarCollapsible
