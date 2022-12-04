87e540228281fd5e9ab2752a6fdd815248c34353
ra11p0 <p.lenczewski@icloud.com>
2022-12-04 15:55:17 +0100

    changed dotnet versions back to 6.0

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello!");
 
 app.MapDefaultControllerRoute();
 
