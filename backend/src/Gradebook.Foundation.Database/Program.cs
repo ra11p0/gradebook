c2b29845093696e940cebe1488b4df119cc4ed5f
ra11p0 <p.lenczewski@icloud.com>
2022-12-09 01:01:20 +0100

    test

--- backend/src/Gradebook.Foundation.Database/Program.cs
+++ backend/src/Gradebook.Foundation.Database/Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Gradebook.Foundation.Database;
+using Microsoft.Extensions.Configuration;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json");
 var config = builder.Build();
 var _ = config.GetConnectionString("DefaultAppDatabase");
+new FoundationDatabaseContext();
