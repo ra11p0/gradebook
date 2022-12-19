4002fdfc44d799ac09a0d8fc0887a659eee089f9
Przemysław Lenczewski <p.lenczewski@icloud.com>
2022-12-19 19:27:08 +0100

    chunks sizes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
+const maxChunks = 5;
+let chunks = 0;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'build',
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          chunks++;
+          return `chunk_${chunks % maxChunks}`;
+        },
+      },
+    },
   },
   server: {
     port: 3005,
