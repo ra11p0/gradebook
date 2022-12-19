d4011e0338be463a44d2b21dd2d959fbb683dba6
Przemysław Lenczewski <p.lenczewski@icloud.com>
2022-12-19 18:35:35 +0100

    chunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,19 @@ export default defineConfig({
   build: {
     outDir: 'build',
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   server: {
     port: 3005,
